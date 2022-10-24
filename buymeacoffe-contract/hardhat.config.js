3f97d492ba2e045b40e7fe4389f649310dfb7a37
Whyat Que <35144180+Whyat@users.noreply.github.com>
2022-10-24 06:36:29 +0000

    week2 dapp+contract code

---
+++ buymeacoffe-contract/hardhat.config.js
@@ -0,0 +1,25 @@
+// hardhat.config.js
+
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const GOERLI = process.env.GOERLI_URL;
+const PRIVATE = process.env.PRIVATE_KEY;
+//console.log(GOERLI);
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: GOERLI,
+      accounts: [PRIVATE]
+    }
+  }
+};
